0030dcb1f599fed90a583b9672a336a0492fa1d9
jetrosuni <jetro.suni@gmail.com>
2019-10-26 20:55:00 +0300

    Display flag icon

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import FlagIcon from 'vue-flag-icon'
 import App from './App.vue'
 
 Vue.use(Buefy)
+Vue.use(FlagIcon)
 
 Vue.config.productionTip = false
 
