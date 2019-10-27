f71a7cbcaf04e66b0af395ea57da59d0f1988cd8
jetrosuni <jetro.suni@gmail.com>
2019-10-27 12:38:07 +0200

    Refactor

---
+++ src/components/BeerItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="beer-item">
+      <flag
+        :iso="item.beer_country"
+        :squared="false"
+      /> <strong>{{item.beer_rating}} ({{item.beer_rating_count}})</strong> {{item.beer_name}} ({{item.beer_style}})</div>
+    <div class="bar-item-area">
+      <div
+        class="bar-item"
+        v-for="(bar, barKey) in barsArray(item)"
+        :key="barKey"
+      >
+        {{bar}}
+        ({{ +daysAgoArray(item)[barKey] === 0 ? 'today' : daysAgoArray(item)[barKey] + ' days ago'}})
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BeerItem",
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    barsArray(item) {
+      return item.bars.split(",");
+    },
+    daysAgoArray(item) {
+      return item.days_ago_bars.split(",");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.bar-item-area {
+  color: #90a4ae; /* blueGrey 300 */
+  margin: 1rem 0 2rem 0;
+}
+</style>
--- src/components/BeerList.vue
+++ src/components/BeerList.vue
@@ -1,43 +1,26 @@
 <template>
   <div class="beer-list">
     <div
-      v-for="(beer, beerKey) in data"
+      v-for="(beerItem, beerKey) in data"
       :key="beerKey"
     >
-      <div class="beer-item">
-        <flag
-          :iso="beer.beer_country"
-          :squared="false"
-        /> <strong>{{beer.beer_rating}} ({{beer.beer_rating_count}})</strong> {{beer.beer_name}} ({{beer.beer_style}})</div>
-      <div class="bar-item-area">
-        <div
-          class="bar-item"
-          v-for="(bar, barKey) in barsArray(beer)"
-          :key="barKey"
-        >
-          {{bar}}
-          ({{ +daysAgoArray(beer)[barKey] === 0 ? 'today' : daysAgoArray(beer)[barKey] + ' days ago'}})
-        </div>
-      </div>
+      <BeerItem :item="beerItem" />
     </div>
   </div>
 </template>
 
 <script>
+import BeerItem from "@/components/BeerItem.vue";
+
 export default {
   name: "BeerList",
+  components: {
+    BeerItem
+  },
   props: {
     data: {
       type: Array,
       required: true
-    }
-  },
-  methods: {
-    barsArray(item) {
-      return item.bars.split(",");
-    },
-    daysAgoArray(item) {
-      return item.days_ago_bars.split(",");
     }
   }
 };
@@ -47,9 +30,5 @@ export default {
 .beer-list {
   text-align: left;
   margin: 4rem 0;
-}
-.bar-item-area {
-  color: #90a4ae; /* blueGrey 300 */
-  margin: 1rem 0 2rem 0;
 }
 </style>
