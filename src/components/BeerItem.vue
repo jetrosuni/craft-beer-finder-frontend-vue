35a8b6f324352123fe46ae74dbcc4bc345306f5e
jetrosuni <jetro.suni@gmail.com>
2019-10-27 18:44:34 +0200

    Improved beer list

--- src/components/BeerItem.vue
+++ src/components/BeerItem.vue
@@ -1,40 +1,50 @@
 <template>
-  <div class="columns">
-    <div class="column is-three-fifths">
-      <div class="columns">
-        <div class="column is-one-quarter cbf-first-col">
-          <div class="columns is-gapless">
-            <div class="column is-half">
-              <div class="cbf-flag">
-                <flag
-                  :iso="item.beer_country"
-                  :squared="false"
-                />
-              </div>
-            </div>
-            <div class="column is-half">
-              <div class="cbf-pint">
-                <img :src="tulipGlassSvg(item.beer_style)" width="18px" />
-              </div>
-            </div>
-          </div>
-          <div class="cbf-rating">
-            <strong>{{item.beer_rating}}</strong> ({{item.beer_rating_count}})
+  <div class="columns cbf-margin-slim">
+    <div class="column is-one-fifth cbf-first-col">
+      <!-- inner 1st column -->
+      <div class="columns is-gapless">
+        <div class="column is-two-fifths">
+          <div class="cbf-flag">
+            <flag
+              :iso="item.beer_country"
+              :squared="false"
+            />
           </div>
         </div>
-        <div class="column cbf-beer-item">
+        <div class="column is-two-fifths">
+          <div class="cbf-pint">
+            <img
+              :src="tulipGlassSvg(item.beer_style)"
+              class="cbf-tulip"
+            />
+          </div>
+        </div>
+        <div class="column is-one-fifth">
+          <div class="cbf-rating">
+            <strong>{{item.beer_rating}}</strong>
+            <div class="is-size-7">
+              {{item.beer_rating_count}}
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /end -->
+    </div>
+    <div class="column is-four-fifths cbf-second-col">
+      <div class="columns cbf-beer-name">
+        <div class="column is-full cbf-remove-padding">
           {{item.beer_name}}
         </div>
       </div>
-    </div>
-    <div class="column is-two-fifths cbf-bar-item-area">
-      <div
-        class="cbf-bar-item"
-        v-for="(bar, barKey) in barsArray(item)"
-        :key="barKey"
-      >
-        {{bar}}
-        ({{ +daysAgoArray(item)[barKey] === 0 ? 'today' : daysAgoArray(item)[barKey] + ' days ago'}})
+      <div class="columns is-multine is-gapless cbf-bar-item-area is-size-7">
+        <div
+          class="column is-one-fifth cbf-bar-item"
+          v-for="(bar, barKey) in barsArray(item)"
+          :key="barKey"
+        >
+          {{bar}}
+          ({{ +daysAgoArray(item)[barKey] === 0 ? 'today' : daysAgoArray(item)[barKey] + ' days ago'}})
+        </div>
       </div>
     </div>
   </div>
@@ -72,7 +82,7 @@ export default {
         style.includes("Schwarzbier") ||
         style.includes("Stout")
       ) {
-        return require('@/assets/img/tulip-glass-dark.svg');
+        return require("@/assets/img/tulip-glass-dark.svg");
       } else if (
         style.includes("Cider") ||
         style.includes("Fruit") ||
@@ -86,7 +96,7 @@ export default {
         style.includes("Spiced") ||
         style.includes("Wild")
       ) {
-        return require('@/assets/img/tulip-glass.svg');
+        return require("@/assets/img/tulip-glass.svg");
       } else if (
         style.includes("Blonde") ||
         style.includes("IPA") ||
@@ -112,7 +122,7 @@ export default {
         style.includes("Saison") ||
         style.includes("Extra Special / Strong Bitter")
       ) {
-        return require('@/assets/img/tulip-glass-light.svg');
+        return require("@/assets/img/tulip-glass-light.svg");
       }
     }
   }
@@ -121,28 +131,39 @@ export default {
 
 <style scoped>
 .cbf-first-col {
+  padding: 0;
   text-align: center;
 }
 .cbf-flag {
   float: right;
-  font-size: 29px;
-  margin-right: 0.5rem;
+  font-size: 36px;
+  margin-right: 0;
 }
 .cbf-pint {
-  float: left;
-  margin-top: 0.35rem;
-  margin-left: 0.5rem;
+  margin-top: 0.3rem;
+}
+.cbf-tulip {
+  width: 24px;
 }
 .cbf-rating {
-  margin-bottom: 2rem;
+  float: left;
+  margin-top: 0.2rem;
+  text-align: center;
 }
-.cbf-beer-item {
-  margin-bottom: 2rem;
+.cbf-remove-padding {
+  padding: 0;
+}
+.cbf-second-col {
+  padding: 0.1rem 0 0 1rem;
+}
+.cbf-beer-name {
+  padding: 0;
+  margin: 0;
 }
 .cbf-bar-item-area {
-  color: #90a4ae; /* blueGrey 300 */
+  color: #757575; /* grey 600 */
 }
-.cbf-bar-item {
-  margin-bottom: 1rem;
+.cbf-margin-slim {
+  margin-bottom: 2rem;
 }
 </style>
