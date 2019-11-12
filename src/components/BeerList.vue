881995a4baf6d7e26c4ee732fed93157e461f1f9
jetrosuni <jetro.suni@gmail.com>
2019-11-12 19:05:56 +0200

    Add CSS transitions

--- src/components/BeerList.vue
+++ src/components/BeerList.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="beer-list">
-    <div
-      v-for="(beerItem, beerKey) in data"
-      :key="beerKey"
+    <transition-group
+      tag="div"
+      name="beer-list"
     >
-      <BeerItem :item="beerItem" />
-    </div>
+      <div
+        v-for="beerItem in data"
+        :key="beerItem.beer_id"
+      >
+        <BeerItem :item="beerItem" />
+      </div>
+    </transition-group>
   </div>
 </template>
 
@@ -30,5 +35,37 @@ export default {
 .beer-list {
   text-align: left;
   margin: 4rem 0;
+}
+
+/* transitions */
+
+.beer-list-enter-active,
+.beer-list-leave-active,
+.beer-list-move {
+  transition-duration: 300ms;
+  transition-timing-function: ease-out;
+  transition-property: opacity, transform;
+}
+
+.beer-list-enter {
+  opacity: 0;
+  transform: translateX(10rem);
+}
+
+.beer-list-enter-to {
+  opacity: 1;
+  transform: translateX(0rem);
+}
+
+.beer-list-leave {
+  opacity: 1;
+}
+
+.beer-list-leave-active {
+  display: none;
+}
+
+.beer-list-leave-to {
+  opacity: 0;
 }
 </style>
