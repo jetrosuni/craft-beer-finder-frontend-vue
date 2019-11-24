828cc47e39acd8b4fdff3fcf880c1e4e4296d58a
jetrosuni <jetro.suni@gmail.com>
2019-11-24 19:29:42 +0200

    Even smaller filter component area on mobile

--- src/components/BeerFilters.vue
+++ src/components/BeerFilters.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cbf-filters">
 
-    <div :class="[ isBeerSearch ? 'is-ignored level is-mobile' : 'level is-mobile']">
+    <div :class="[ isBeerSearch ? 'cbf-is-ignored cbf-beer-style-area level is-mobile' : 'cbf-beer-style-area level is-mobile']">
       <div class="level-left">
         <div class="level-item cbf-style-label">Beer style</div>
         <div class="block level-item">
@@ -41,7 +41,7 @@
       </div>
     </div>
 
-    <div :class="[ isBeerSearch || isVenueSearch ? 'is-ignored level is-mobile' : 'level is-mobile']">
+    <div :class="[ isBeerSearch || isVenueSearch ? 'cbf-is-ignored level is-mobile' : 'level is-mobile']">
       <div class="cbf-rating-label">
         Rating scale
       </div>
@@ -65,25 +65,31 @@
     </div>
 
     <div class="columns">
-      <div class="column is-half">
-        <b-input
-          v-model="searchStr"
-          type="is-light"
-          placeholder="Find a beer"
-          @input="option => onSearchStringChanged(option)"
-        />
+      <div class="column is-half cbf-input-area">
+        <b-field type="is-light">
+          <b-input
+            :size="inputFieldSize"
+            v-model="searchStr"
+            type="is-light"
+            placeholder="Find a beer"
+            @input="option => onSearchStringChanged(option)"
+          />
+        </b-field>
       </div>
-      <div :class="[ isBeerSearch ? 'is-ignored column is-half' : 'column is-half' ]">
-        <b-autocomplete
-          v-model="venueStr"
-          type="is-light"
-          placeholder="Filter by venue"
-          :keep-first="false"
-          :open-on-focus="false"
-          :data="venueNames"
-          field="venue_name"
-          @select="option => onVenueStringChanged(option)"
-        />
+      <div :class="[ isBeerSearch ? 'cbf-is-ignored column is-half cbf-input-area' : 'column is-half cbf-input-area' ]">
+        <b-field type="is-light">
+          <b-autocomplete
+            :size="inputFieldSize"
+            v-model="venueStr"
+            type="is-light"
+            placeholder="Filter by venue"
+            :keep-first="false"
+            :open-on-focus="false"
+            :data="venueNames"
+            field="venue_name"
+            @select="option => onVenueStringChanged(option)"
+          />
+        </b-field>
       </div>
     </div>
   </div>
@@ -112,8 +118,15 @@ export default {
       venueStr: "",
       venueList: [],
       minRating: 4,
-      beerStyleSelection: ["light", "dark", "sour", "other"]
+      beerStyleSelection: ["light", "dark", "sour", "other"],
+      windowInnerWidth: window.innerWidth
     };
+  },
+  created() {
+    window.addEventListener('resize', this.handleResize)
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.handleResize)
   },
   mounted() {
     this.venueList = this.data.reduce((results, beer) => {
@@ -133,6 +146,9 @@ export default {
     }, []);
   },
   computed: {
+    inputFieldSize: function() {
+      return this.windowInnerWidth < 769 ? 'is-small' : 'is-normal'
+    },
     venueNames: function() {
       return this.venueList.reduce((results, venue) => {
         if (
@@ -148,6 +164,9 @@ export default {
     }
   },
   methods: {
+    handleResize() {
+      this.windowInnerWidth = window.innerWidth;
+    },
     onSearchStringChanged(searchStr) {
       this.$emit("change-filter-search-string", searchStr && searchStr.length > 2 ? searchStr : null);
     },
@@ -167,6 +186,7 @@ export default {
 <style scoped>
 .cbf-filters {
   text-align: left;
+  font-size: 0.8rem;
 }
 .cbf-style-label {
   color: #c2c2c2;
@@ -179,8 +199,23 @@ export default {
 .cbf-slider {
   padding: 0 1rem;
 }
-.is-ignored {
+.cbf-beer-style-area {
+  margin-bottom: 0.5rem;
+}
+.cbf-input-area {
+  padding: 0.25rem 0.5rem;
+}
+.cbf-is-ignored {
   text-decoration: line-through;
   opacity: 0.25;
+}
+
+@media only screen and (min-width: 769px) {
+  .cbf-filters {
+    font-size: 1rem;
+  }
+  .cbf-beer-style-area {
+    margin-bottom: 1rem;
+  }
 }
 </style>
