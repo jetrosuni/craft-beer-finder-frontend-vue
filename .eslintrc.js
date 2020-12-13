01f6a4819341cd4757cc1300fe3e2390f98d5686
jetrosuni <jetro.suni@gmail.com>
2020-12-13 17:58:57 +0200

    Disable vue/experimental-script-setup-vars rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/experimental-script-setup-vars': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
