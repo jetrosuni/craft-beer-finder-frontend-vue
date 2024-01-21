d16ae6e00d3e8f5999c811bed725edf2eb3bd4e0
Jetro Suni <jetro.suni@gmail.com>
2024-01-21 17:47:30 +0200

    Upgrade components to Vue 3 and TypeScript + use Tailwind

---
+++ src/components/BeerFilter.vue
@@ -0,0 +1,214 @@
+<template>
+  <section class="mx-3 mt-1.5 pt-3 text-left text-xs sm:mt-3 sm:text-sm md:text-base">
+    <div class="grid grid-cols-7">
+      <BeerFilterStyle
+        :beer-styles="filterValues.beerStylesAll"
+        :selected-styles="filterValues.beerStylesSelected"
+        :is-disabled="isBeerSearch"
+        @beer-style-selected="onBeerStyleSelected"
+      />
+      <DarkModeButton class="cbf-tiny-screen-support ml-auto" />
+    </div>
+    <div class="grid grid-cols-6">
+      <div class="pr-1 pt-5" :class="{ 'cbf-global-is-ignored': isBeerSearch }">
+        <p>Day limit</p>
+        <p>
+          {{ filterValues.dayLimit === 0 ? 'today' : '0–' + filterValues.dayLimit + ' days ago' }}
+        </p>
+      </div>
+      <div
+        class="col-span-5 pt-5"
+        :class="{ 'cbf-global-is-ignored pointer-events-none': isBeerSearch }"
+      >
+        <BeerFilterRange
+          :is-disabled="isBeerSearch"
+          :min="0"
+          :max="7"
+          :step="1"
+          :value="filterValues.dayLimit"
+          @change="(value) => onDayRangeChanged(value)"
+        />
+      </div>
+      <div class="pr-1 pt-5" :class="{ 'cbf-global-is-ignored': isBeerSearch || isVenueSearch }">
+        <p>Rating</p>
+        <p>{{ `${filterValues.ratingMin}–5 stars` }}</p>
+      </div>
+      <div
+        class="col-span-5 pt-5"
+        :class="{ 'cbf-global-is-ignored pointer-events-none': isBeerSearch || isVenueSearch }"
+      >
+        <BeerFilterRange
+          :is-disabled="isBeerSearch || isVenueSearch"
+          :min="3.5"
+          :max="4.5"
+          :step="0.25"
+          :ticks="['3.5', '3.75', '4.0', '4.25', '4.5']"
+          :value="filterValues.ratingMin"
+          @change="(value) => onRatingRangeChanged(value)"
+        />
+      </div>
+      <div class="col-span-3 pr-5 pt-5">
+        <div class="mb-3">
+          <div class="relative mb-4 flex w-full flex-wrap items-stretch">
+            <input
+              type="search"
+              class="relative m-0 block min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.32rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary dark:focus:text-neutral-200"
+              placeholder="Find a beer"
+              aria-label="Find a beer"
+              v-model="searchBeerString"
+              @input="onSearchStringChanged"
+            />
+          </div>
+        </div>
+      </div>
+      <div
+        class="col-span-3 pt-5"
+        :class="{ 'cbf-global-is-ignored pointer-events-none': isBeerSearch }"
+      >
+        <div class="relative" data-te-input-wrapper-init id="venueFilter">
+          <input
+            type="search"
+            class="peer block min-h-[auto] w-full rounded border-neutral-300 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear placeholder:opacity-0 focus:placeholder:opacity-100 peer-focus:border-primary motion-reduce:transition-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:border-primary"
+            id="beerFilterFormVenueInput"
+          />
+          <label
+            for="beerFilterFormVenueInput"
+            class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[80%] origin-[0_0] truncate pt-[0.37rem] text-base leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:border-primary peer-focus:bg-white peer-data-[te-input-focused]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-focused]:scale-[0.8] peer-data-[te-input-state-active]:scale-[0.8] peer-data-[te-input-focused]:bg-white peer-data-[te-input-state-active]:bg-white motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:border-primary dark:peer-focus:bg-neutral-900 dark:peer-data-[te-input-focused]:bg-neutral-900 dark:peer-data-[te-input-state-active]:bg-neutral-900"
+            >Filter by venue
+          </label>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { type Beer, type FilterValues, type Venue } from '../types'
+
+import { defineComponent, type PropType } from 'vue'
+import debounce from 'lodash.debounce'
+import { Autocomplete, Input, initTE } from 'tw-elements'
+
+import BeerFilterRange from '@/components/BeerFilterRange.vue'
+import BeerFilterStyle from '@/components/BeerFilterStyle.vue'
+import DarkModeButton from '@/components/DarkModeButton.vue'
+
+export default defineComponent({
+  name: 'BeerFilter',
+  components: {
+    BeerFilterRange,
+    BeerFilterStyle,
+    DarkModeButton,
+  },
+  props: {
+    beerList: {
+      type: Array as PropType<Readonly<Array<Beer>>>,
+      required: true,
+    },
+    venues: {
+      type: Array as PropType<Readonly<Array<Venue>>>,
+      required: true,
+    },
+    filterValues: {
+      type: Object as PropType<FilterValues>,
+      required: true,
+    },
+    isBeerSearch: {
+      type: Boolean,
+      required: true,
+    },
+    isVenueSearch: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      searchBeerString: '',
+      searchVenueString: '',
+    }
+  },
+  computed: {
+    venueNames(): Array<string> {
+      return this.venues.map((venue) => venue.name)
+    },
+  },
+  mounted() {
+    initTE({ Input })
+
+    this.emitDayRangeChange = debounce(this.emitDayRangeChange, 320)
+    this.emitRatingChange = debounce(this.emitRatingChange, 320)
+    this.onSearchStringChanged = debounce(this.onSearchStringChanged, 320)
+
+    const venueAutocomplete = document.querySelector('#venueFilter')
+
+    const dataFilter = (input: string) =>
+      this.venueNames.filter((venue: string) => venue.toLowerCase().includes(input.toLowerCase()))
+
+    new Autocomplete(venueAutocomplete, {
+      filter: dataFilter,
+      noResults: 'No venue found.',
+    })
+
+    const venueFilter = document.getElementById('venueFilter')
+
+    if (venueFilter) {
+      const emitVenueChange = (e: CustomEvent) => {
+        // NOTE: value property doesn't exist in CustomEvent as it's unique for TW Elements Autocomplete component
+        const selection = (e as any).value
+
+        this.$emit('change-filter-venue-string', this.ensureMinimumInputLength(selection))
+      }
+
+      venueFilter.addEventListener('itemSelect.te.autocomplete', emitVenueChange as EventListener)
+      venueFilter.addEventListener('update.te.autocomplete', emitVenueChange as EventListener)
+    }
+  },
+  methods: {
+    ensureMinimumInputLength(str: string | undefined): string | undefined {
+      return str?.length && str.length > 2 ? str : undefined
+    },
+    emitDayRangeChange(dayLimit: number): void {
+      this.$emit('change-filter-day-range', dayLimit)
+    },
+    emitRatingChange(minRating: number): void {
+      this.$emit('change-filter-rating-range', minRating)
+    },
+    onSearchStringChanged(): void {
+      this.$emit(
+        'change-filter-search-string',
+        this.ensureMinimumInputLength(this.searchBeerString),
+      )
+    },
+    onBeerStyleSelected(beerStyleArray: Array<string>): void {
+      this.$emit('change-filter-beer-style', beerStyleArray)
+    },
+    onDayRangeChanged(dayLimit: number): void {
+      this.emitDayRangeChange(dayLimit)
+    },
+    onRatingRangeChanged(minRating: number): void {
+      this.emitRatingChange(minRating)
+    },
+  },
+})
+</script>
+
+<style scoped>
+.cbf-day-label,
+.cbf-rating-label {
+  color: #c2c2c2;
+  min-width: 6rem;
+  width: 6rem;
+}
+.cbf-input-area {
+  padding: 0.25rem 0.5rem;
+}
+
+@media only screen and (max-width: 400px) {
+  .cbf-tiny-screen-support {
+    position: fixed;
+    top: 1rem;
+    right: 1rem;
+  }
+}
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,257 @@
+<template>
+  <section class="mt-6 text-center lg:mt-10">
+    <h1 class="text-2xl font-bold sm:mb-3 lg:mb-5 lg:text-4xl xl:text-5xl">
+      {{ titleText }}
+    </h1>
+    <div v-if="isDisplayLoadingIcon && isLoading">
+      <div class="cbf-loading">
+        <BeerLoading />
+      </div>
+    </div>
+    <div v-if="!!errorMessage" class="mb-3 text-red-600">{{ errorMessage }}</div>
+    <div
+      class="sticky top-0 z-50 border-b-4 border-neutral-200 bg-white dark:border-neutral-800 dark:bg-neutral-900"
+      v-if="!isLoading && venues && !!!errorMessage"
+    >
+      <BeerFilter
+        :beer-list="beerList"
+        :venues="venues"
+        :filter-values="filterValues"
+        :is-beer-search="isBeerNameSearch"
+        :is-venue-search="isVenueSearch"
+        @change-filter-search-string="onFilterSearchStringChanged"
+        @change-filter-venue-string="onFilterVenueStringChanged"
+        @change-filter-beer-style="onFilterBeerStyleChanged"
+        @change-filter-day-range="onFilterDayRangeChanged"
+        @change-filter-rating-range="onFilterRatingRangeChanged"
+      />
+    </div>
+    <BeerList
+      v-if="isBeerListItems"
+      :beer-list="filteredBeerList"
+      :day-limit="isBeerNameSearch || isVenueSearch ? 7 : filterValues.dayLimit"
+    />
+    <div v-if="!isDisplayLoadingIcon && !isFullListLoaded" class="relative mt-6 pb-24 italic">
+      <p>Serving more beer ... please wait ...</p>
+      <div class="absolute left-2/4 top-0 md:top-[-2.5rem]">
+        <BeerLoading size="small" />
+      </div>
+    </div>
+    <template v-if="isFullListLoaded && !isLoading">
+      <div v-if="!filteredBeerList?.length" class="my-6">
+        No results found with the selected filters.
+      </div>
+      <footer v-else class="my-8 text-xs font-bold">Data provided by Untappd</footer>
+    </template>
+  </section>
+</template>
+
+<script lang="ts">
+import { type Beer, type FilterValues, type Venue } from '../types'
+
+import { defineComponent, readonly } from 'vue'
+
+import BeerFilter from '@/components/BeerFilter.vue'
+import BeerList from '@/components/BeerList.vue'
+import BeerLoading from '@/components/BeerLoading.vue'
+import { backendService } from '@/services/backendService'
+
+export default defineComponent({
+  name: 'MainView',
+  components: {
+    BeerFilter,
+    BeerList,
+    BeerLoading,
+  },
+  data() {
+    return {
+      refreshTimer: <number | undefined>undefined,
+      errorMessage: <string | undefined>undefined,
+      genericErrorMessage: 'Connection failed ... please try again later ...',
+      beerList: <Readonly<Array<Beer>>>[],
+      venues: <Readonly<Array<Venue>>>[],
+      waitingForResponse: false,
+      isDisplayLoadingIcon: true,
+      isLoading: false,
+      isFullListLoaded: false,
+      filterValues: <FilterValues>{
+        searchBeerString: '',
+        searchVenueString: '',
+        dayLimit: 5,
+        ratingMin: 3.75,
+        beerStylesAll: ['light', 'dark', 'sour', 'other'],
+        beerStylesSelected: <Array<string>>[],
+      },
+    }
+  },
+  mounted() {
+    this.filterValues.beerStylesSelected = this.filterValues.beerStylesAll
+
+    this.isDisplayLoadingIcon = true
+    this.isFullListLoaded = false
+
+    this.requestVenues()
+    this.requestBeers(true, false)
+
+    this.refreshTimer = setInterval(() => {
+      this.requestBeers(false, true)
+    }, 1200000)
+  },
+  beforeDestroy() {
+    clearInterval(this.refreshTimer)
+  },
+  computed: {
+    titleText(): string {
+      return import.meta.env.VITE_CRAFT_BEER_FINDER_TITLE
+    },
+    filteredBeerNames(): Array<Beer> {
+      return this.beerList.filter((beer) =>
+        this.isBeerNameSearch
+          ? beer.name.toLowerCase().includes(this.filterValues.searchBeerString.toLowerCase())
+          : true,
+      )
+    },
+    filteredVenues(): Array<Beer> {
+      return this.filteredBeerNames.filter((beer) =>
+        this.isVenueSearch
+          ? beer.venues.some((venue) =>
+              venue.name.toLowerCase().includes(this.filterValues.searchVenueString.toLowerCase()),
+            )
+          : true,
+      )
+    },
+    filteredLightBeers(): Array<Beer> {
+      return this.filteredVenues.filter((beer) =>
+        !this.filterValues.beerStylesSelected.includes('light') ? beer.style !== 'light' : true,
+      )
+    },
+    filteredDarkBeers(): Array<Beer> {
+      return this.filteredLightBeers.filter((beer) =>
+        !this.filterValues.beerStylesSelected.includes('dark') ? beer.style !== 'dark' : true,
+      )
+    },
+    filteredSourBeers(): Array<Beer> {
+      return this.filteredDarkBeers.filter((beer) =>
+        !this.filterValues.beerStylesSelected.includes('sour') ? beer.style !== 'sour' : true,
+      )
+    },
+    filteredOtherBeers(): Array<Beer> {
+      return this.filteredSourBeers.filter((beer) =>
+        !this.filterValues.beerStylesSelected.includes('other') ? beer.style !== 'other' : true,
+      )
+    },
+    filteredDayLimit(): Array<Beer> {
+      return this.filteredOtherBeers.filter(
+        (beer) => !!beer.venues.some((venue) => venue.daysAgo <= this.filterValues.dayLimit),
+      )
+    },
+    filteredBeerList(): Array<Beer> {
+      // NOTE: if beer name search is active, do not care about the other filters
+      return this.isBeerNameSearch
+        ? this.filteredBeerNames
+        : this.filteredDayLimit.filter((beer) =>
+            this.isVenueSearch ? true : beer.rating >= this.filterValues.ratingMin,
+          )
+    },
+    isBeerListItems(): boolean {
+      return !!(!this.waitingForResponse && this.filteredBeerList && this.filteredBeerList.length)
+    },
+    isBeerNameSearch(): boolean {
+      return !!(this.filterValues.searchBeerString && this.filterValues.searchBeerString.length > 2)
+    },
+    isVenueSearch(): boolean {
+      return !!(
+        this.filterValues.searchVenueString && this.filterValues.searchVenueString.length > 2
+      )
+    },
+  },
+  methods: {
+    requestVenues(): void {
+      backendService
+        .getVenues()
+        .then((res) => {
+          this.venues = res
+        })
+        .catch((err) => {
+          console.log(err)
+
+          this.errorMessage = this.genericErrorMessage
+        })
+    },
+    requestBeers(initialDataOnly = false, isSilent = false): void {
+      if (this.waitingForResponse) {
+        return
+      }
+
+      if (!isSilent) {
+        this.isLoading = true
+      }
+
+      if (initialDataOnly) {
+        backendService
+          .getTopBeers()
+          .then((res) => {
+            this.resolveRequest(res)
+
+            this.isDisplayLoadingIcon = false
+          })
+          .catch((err) => {
+            console.log(err)
+
+            this.errorMessage = this.genericErrorMessage
+          })
+          .finally(() => {
+            this.waitingForResponse = false
+            this.isLoading = false
+          })
+
+        return
+      }
+
+      backendService
+        .getAllBeers()
+        .then((res) => {
+          this.resolveRequest(res)
+
+          this.isFullListLoaded = true
+        })
+        .catch((err) => {
+          console.log(err)
+
+          this.errorMessage = this.genericErrorMessage
+        })
+        .finally(() => {
+          this.waitingForResponse = false
+          this.isLoading = false
+        })
+    },
+    resolveRequest(response: Array<Beer>): void {
+      if (response?.[0]?.beerId && response[0].beerId > 0) {
+        this.beerList = readonly(response)
+      }
+
+      if (!this.isFullListLoaded) {
+        // NOTE: Wait a moment before executing to make sure the DOM gets refreshed
+        setTimeout(() => {
+          this.requestBeers(false, true)
+        }, 2500)
+      }
+    },
+    onFilterSearchStringChanged(searchStr: string): void {
+      this.filterValues.searchBeerString = searchStr
+    },
+    onFilterVenueStringChanged(venueStr: string): void {
+      this.filterValues.searchVenueString = venueStr
+    },
+    onFilterBeerStyleChanged(beerStyleArray: Array<string>): void {
+      this.filterValues.beerStylesSelected = beerStyleArray
+    },
+    onFilterDayRangeChanged(dayLimit: number): void {
+      this.filterValues.dayLimit = dayLimit
+    },
+    onFilterRatingRangeChanged(minRating: number): void {
+      this.filterValues.ratingMin = minRating
+    },
+  },
+})
+</script>
