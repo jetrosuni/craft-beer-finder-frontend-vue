c4931975511ec36eb16ecefc7b1afba166625594
Jetro Suni <jetro.suni@gmail.com>
2024-01-23 00:27:53 +0200

    Fix field id

--- src/components/BeerFilter.vue
+++ src/components/BeerFilter.vue
@@ -51,6 +51,7 @@
         <div class="mb-3">
           <div class="relative mb-4 flex w-full flex-wrap items-stretch">
             <input
+              id="search-field"
               type="search"
               class="relative m-0 block min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.32rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary dark:focus:text-neutral-200"
               placeholder="Find a beer"
--- src/components/DarkModeButton.vue
+++ src/components/DarkModeButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <input type="checkbox" name="light-switch" v-model="isDark" class="light-switch sr-only" />
+    <input type="checkbox" id="light-switch" v-model="isDark" class="light-switch sr-only" />
     <label
       class="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full bg-slate-100 hover:bg-slate-200 dark:bg-slate-700 dark:hover:bg-slate-600/80"
       for="light-switch"
