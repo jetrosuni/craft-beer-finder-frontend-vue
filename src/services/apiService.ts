56d85bfd2d68b759931fefa958ec32de5dde618d
Jetro Suni <jetro.suni@gmail.com>
2024-01-22 17:31:13 +0200

    Set RequestInit type

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,7 +2,7 @@ export const apiService = {
   get(resource: string) {
     const url = import.meta.env.VITE_CRAFT_BEER_FINDER_API_URL + resource
 
-    return fetch(url, <any>{
+    return fetch(url, <RequestInit>{
       Accept: 'application/json',
       'Content-Type': 'application/json;charset=UTF-8',
     }).then((response) => response.json())
